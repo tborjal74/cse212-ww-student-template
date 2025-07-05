5d3acad8425722645fb64066dc811daf493f0781
tborjal <tborjal@byupathway.edu>
2025-07-05 13:10:26 +0800

    finished multiples of code assignment

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,21 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
+        double[] multiples = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+        
+        return multiples;
+        
         // TODO Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // return []; replace this return statement with your own
     }
 
     /// <summary>
